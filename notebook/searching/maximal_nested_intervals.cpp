db8409f2865d00720778878d37f3a61007f07107
Romelai28 <Romelai28@gmail.com>
2025-05-23 17:01:22 -0300

    elimino tildes que eran problematicas con la compilacion a pdf

--- notebook/searching/maximal_nested_intervals.cpp
+++ notebook/searching/maximal_nested_intervals.cpp
@@ -7,8 +7,8 @@ bool compare(const intervalo_t & a, const intervalo_t & b) {
     return a.start > b.start;
 }
 
-// Calcula, dado un conjunto de intervalos, la máxima cantidad de intervalos que cumplen que formen una cadena de contención.  // O(n*log(n))
-// Donde la contención es NO es estricta. Asume que el vector intervals sea no vacio.
+// Calcula, dado un conjunto de intervalos, la maxima cantidad de intervalos que cumplen que formen una cadena de contencion.  // O(n*log(n))
+// Donde la contencion es NO es estricta. Asume que el vector intervals sea no vacio.
 // Algoritmo basado en calcular la LIS de los puntos finales una vez ordenados los intervalos.
 int calcular_maximal_nested_intervals(vector<intervalo_t> &intervals){
 	sort(all(intervals), compare);
