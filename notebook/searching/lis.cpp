251ee836df2f75884e991149675fefa76d6f1c5a
Romelai28 <Romelai28@gmail.com>
2025-05-23 16:19:55 -0300

    lis

--- notebook/searching/lis.cpp
+++ notebook/searching/lis.cpp
@@ -2,9 +2,8 @@
 // Caso *estrictamente* creciente.
 int calcular_LIS(vector<int> &valores){
 	vector<int> lis;
-	
 	lis.pb(valores[0]);
-	
+
 	for(int i=1; i<n; i++){
 		if(lis.back() < valores[i]){  // Para el caso creciente pero no estricto, poner <= y usar upper_bound.
 			lis.pb(valores[i]);
@@ -12,8 +11,7 @@ int calcular_LIS(vector<int> &valores){
 			int low = lower_bound(all(lis), valores[i]) - lis.begin();
 			lis[low] = valores[i];
 		}
-		
 	}
-	
+
 	return SIZE(lis);
 }
