79a4d3705de1ca632619f49ef4289915032672c3
Romelai28 <Romelai28@gmail.com>
2025-05-27 04:21:37 -0300

    agrego toposort de kahn

---
+++ notebook/graph/toposort_kahn.cpp
@@ -0,0 +1,34 @@
+// Devuelve true sii existe un ciclo en G.
+// Si no existe ciclo, en orden queda almacenado un orden topologico de G.
+bool toposort(vector<vector<int>> &ady, vector<int> &orden){
+	orden.clear();
+	vector<int> in_deg(SIZE(ady), 0);
+	
+	for(int v=0; v<SIZE(ady); v++){
+		for(const auto u : ady[v]){
+			in_deg[u]++;
+		}
+	}
+	
+	queue<int> q;
+	for(int v=0; v<SIZE(ady); v++){
+		if(in_deg[v] == 0){
+			q.push(v);
+		}
+	}
+	
+	while(!q.empty()){
+		int v = q.front();
+		q.pop();
+		orden.pb(v);
+		
+		for(auto u : ady[v]){
+			in_deg[u]--;
+			if(in_deg[u] == 0){
+				q.push(u);
+			}
+		}
+	}
+	
+	return SIZE(orden) != SIZE(ady);  // Retorna true sii hay un ciclo.
+}
