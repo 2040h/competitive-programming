fdde0f47b520b1cc1aa68bfefb43a9fb3d155f61
FacundoCG <coronelfacundo30@gmail.com>
2025-05-30 21:05:40 -0300

    Agregar UNDEFINED al binaryJumping

--- notebook/data_structures/binary_jumping.cpp
+++ notebook/data_structures/binary_jumping.cpp
@@ -6,7 +6,7 @@ struct BinaryJumping{
 	// Inicializacion en tiempo O(n*log(max_k))
 	BinaryJumping(vector<int> &succ, int max_k){
 		logk = log2(max_k);  // Piso.
-		binaryJumping.assign(SIZE(succ), vector<int>(logk+1));
+		binaryJumping.assign(SIZE(succ), vector<int>(logk+1, UNDEFINED));
 		
 		for(int i=0; i<SIZE(succ); i++){
 			binaryJumping[i][0] = succ[i];
@@ -14,23 +14,25 @@ struct BinaryJumping{
 		
 		for(int it=1; it<=logk; it++){
 			for(int i=0; i<SIZE(succ); i++){
-				binaryJumping[i][it] = binaryJumping[binaryJumping[i][it-1]][it-1];
+				if (binaryJumping[i][it-1] != UNDEFINED) binaryJumping[i][it] = binaryJumping[binaryJumping[i][it-1]][it-1];
 			}
 		}
 	}
 	
 	// O(log(k)). Donde x es el nodo, k es la distancia de salto.
 	int succ_k(int x, int k){
+		// Suponemos que k >= 0
+
 		int it = 0;
 		while(k != 0){
 			if ((k & 1) == 1){
-				x = binaryJumping[x][it];
+				if (x != UNDEFINED) x = binaryJumping[x][it];
 			}
 			k = k >> 1;
 			it++;
 		}
 		
-		return x;
+		return x; // x = UNDEFINED significa que te excediste con los saltos
 	}
 	
 };
