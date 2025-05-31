0ddfdd6d08a3dfc044b717894de26fe4f4e65298
Romelai28 <Romelai28@gmail.com>
2025-05-31 02:08:40 -0300

    enumerar abecedario ya no usa inmediatos numericos

--- notebook/other/enumerar_abecedario.cpp
+++ notebook/other/enumerar_abecedario.cpp
@@ -1,3 +1,2 @@
-// 'A' es 65 | 'a' es 97.
-int transformarLetra(char x) {return (int) (x - 97);}
-char transformarNumero(int x) {return (char) (x + 97);}
+int transformarLetra(char x) {return (int) (x - 'a');}
+char transformarNumero(int x) {return (char) (x + 'a');}
