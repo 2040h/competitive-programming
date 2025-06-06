84552d60703f808f437cb7760ccbbac102fe123f
Romelai28 <Romelai28@gmail.com>
2025-06-05 22:02:20 -0300

    scc

---
+++ notebook/graph/scc.cpp
@@ -0,0 +1,61 @@
+struct SCC {
+	int n, comps = 0;
+	vb vis;
+	vi order, id_scc;
+	vector<vi> ady, ady_t;
+	vi representante;  // (Opcional) Dada un *id_scc*, te dice algun nodo de esa scc.
+	
+	SCC(vector<vi> &_ady, vector<vi> &_ady_t){
+		ady = _ady;
+		ady_t = _ady_t;
+		n = SIZE(ady);
+		vis.assign(n, false);
+		id_scc.resize(n);
+		
+		for(int i=0; i<n; i++){
+			if(!vis[i]) { dfs1(i); }
+		}
+		vis.assign(n, false);
+		reverse(all(order));
+		for(int v : order){
+			if(!vis[v]){
+				dfs2(v, comps);
+				representante.pb(v);
+				comps++;  // Aumento el contador de SCC.
+			}
+		}
+	}
+	
+	vector<vi> sccToDAG(){
+		vector<set<int>> ady_dag_with_set(comps);
+		forn(v, n){
+			for(int u : ady[v]){
+				ady_dag_with_set[id_scc[v]].insert(id_scc[u]);
+			}
+		}
+		// Convertir a vector de vector de int.
+		vector<vi> ady_dag;
+		for(const auto &s : ady_dag_with_set){
+			vi tmp(all(s));
+			ady_dag.pb(tmp);
+		}
+		return ady_dag;
+	}
+
+private:
+	void dfs1(int v) {
+		vis[v] = true;
+		for(int u : ady[v]) {
+			if (!vis[u]) { dfs1(u); }
+		}
+		order.push_back(v);
+	}
+    
+	void dfs2(int x, int comp) {
+		vis[x] = true;
+		for (int u : ady_t[x]) {
+			if (!vis[u]) { dfs2(u, comp); }
+		}
+		id_scc[x] = comp;
+	}
+};
