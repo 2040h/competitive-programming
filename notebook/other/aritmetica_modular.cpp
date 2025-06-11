b14837aa3f72e6ba6a4a44f5a710d2a071eb44ee
FacundoCG <coronelfacundo30@gmail.com>
2025-06-11 19:33:59 -0300

    Cambios en funciones_utiles, gcd, aritmetica_modular y funciones_utiles en matematica

--- notebook/other/aritmetica_modular.cpp
+++ notebook/other/aritmetica_modular.cpp
@@ -24,3 +24,12 @@ int divideMod(int a, int b, int m = MOD){
     int res = mulMod(a % m, binPowMod(b, m-2, m) % m, m) % m;
     return res;
 }
+
+void calcularFactorial(int n){
+	fact[0] = 1;
+	forsn(i, 1, n+1) fact[i] = mulMod(fact[i-1], i, MOD);
+}
+
+int combinatorial_func(int n, int k){
+	return divideMod(fact[n], mulMod(fact[k], fact[n-k], MOD), MOD);
+}
