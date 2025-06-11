8935bf9410c6adf730df07677c51874b323e185c
FacundoCG <coronelfacundo30@gmail.com>
2025-06-11 20:43:28 -0300

    Actualizacion binary search

--- notebook/searching/binary_search.cpp
+++ notebook/searching/binary_search.cpp
@@ -1,29 +1,60 @@
 // Asumiento que quiero hacer una busqueda binaria en el rango [0, n)
 // Importante: chequear el indice porque en caso de que no exista elemento que cumple P(X) puede haber problemas
 
-// Calcular extremo derecho que cumple P(X)
-int l = -1; // extremo izquierdo del rango de busqueda -1
-int r = n; // extremo derecho del rango de busqueda +1
-while(r - l > 1) { // mientras que la distancia entre las fronteras sea >1 (es decir, mientras que no esten contiguas)
-    int mid = (l + r) / 2;
-    if(P(mid)) {
-        l = mid;
+// Encontrar el indice del elemento v en el array A ordenado
+int binarySearch(int start, int end, vector<ll> &A, ll v){
+    if (start > end){
+        return -1;
+    }
+
+    int mid = (start + end)/2;
+
+    if (A[mid] == v){
+        return mid;
+    } else if (A[mid] < v){
+        return binarySearch(mid+1, end, A, v);
     } else {
-        r = mid;
+        return binarySearch(start, mid-1, A, v);
     }
 }
-// l es el ultimo elemento que cumple P(X)
+
+// Calcular extremo derecho que cumple P(X)
+int rightBinarySearch(int start, int end){
+    int l = start - 1;
+    int r = end + 1;
+
+    while (r - l > 1){
+        int mid = (l + r)/2;
+
+        if (P(mid)){
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+    
+    if (l < start || l > end || !P(l)) l = -1; // Si l no está en el intervalo [start,end] o no cumple la propiedad, entonces retorno -1
+	
+    return l; // l es el ultimo elemento que cumple P(X)
+}
 
 // Calcular extremo izquierdo que cumple P(X)
-int l = -1; 
-int r = n; 
-while(r - l > 1) { 
-    int mid = (l + r) / 2;
-    if(!P(mid)) {
-        l = mid;
-    } else {
-        r = mid;
+int leftBinarySearch(int start, int end){
+    int l = start - 1; 
+    int r = end + 1; 
+
+    while(r - l > 1) { 
+        int mid = (l + r) / 2;
+        if(!P(mid)) {
+            l = mid;
+        } else {
+            r = mid;
+        }
     }
+    
+    if (r < start || r > end || !P(r)) r = -1; // Si r no está en el intervalo [start,end] o no cumple la propiedad, entonces retorno -1
+
+    return r; // r es el primer elemento que cumple P(X)
 }
 
-// r es el primer elemento que cumple P(X)
+
