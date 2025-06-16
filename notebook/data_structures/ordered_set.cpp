135b4fc914e6a13ba2cdd9dc61f2b50b623f994d
FacundoCG <coronelfacundo30@gmail.com>
2025-06-16 03:23:47 -0300

    HashTable

--- notebook/data_structures/ordered_set.cpp
+++ notebook/data_structures/ordered_set.cpp
@@ -3,7 +3,19 @@
 using namespace __gnu_pbds; 
 
 #define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update> 
+template <typename T>
+using ordered_multiset = tree<
+    pair<T, int>,
+    null_type,
+    less<pair<T, int>>, // NOT less_equal
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
 
 ordered_set v;
 v.insert(0);
 *v.find_by_order(k); // Elemento en la posición k-th
+
+ordered_multiset<T> s;
+s.insert({t, i}); // T es el tipo del primer elemento del pair. El segundo es un indice para definir una relación de orden estricto (se rompe sin esto)
+
