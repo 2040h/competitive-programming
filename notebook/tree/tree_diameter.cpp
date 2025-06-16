fa8519a2bea59e40fb867480609b0d270b9c890c
FacundoCG <coronelfacundo30@gmail.com>
2025-06-16 14:19:14 -0300

    Tree diameter corregido

--- notebook/tree/tree_diameter.cpp
+++ notebook/tree/tree_diameter.cpp
@@ -1,69 +1,57 @@
-// Versión con DFS
+// Versión con BFS
 
-// Tengo que tener calculado previamente el nivel de cada vértica para una raíz cualquiera
+int diameterVertex;
 
-struct TreeDiameter{
-    vector<vector<int>> adjList;
-    vector<int> levelOfNode;
-    vector<bool> visited;
+int maxDepth(int v){
+    visited.assign(m, false);
+    distances.assign(m, INF);
+    distances[v] = 0;
+    visited[v] = true;
 
-    TreeDiameter(vector<vector<int>> &adjListArg){
-        adjList = adjListArg;
-        levelOfNode.resize(SIZE(adjList), UNDEFINED);
-        visited.resize(SIZE(adjList), false);
-    }
+    queue<int> q;
+    q.push(v);
 
-    void calculateLevels(int v, int currentLevel){
-        levelOfNode[v] = currentLevel;
-        visited[v] = true;
-
-        for (int u : adjList[v]){
-            if (!visited[u]) calculateLevels(u, currentLevel+1);
-        }
-    }
-
-
-    int getOneVertexOfDiameter(){
-        visited.assign(n, false); // Por las dudas marco todo como no visitado antes de correr el DFS
-        calculateLevelsFrom(0, 0);
-        int res = UNDEFINED;
-        int currentLevel = -1;
-
-        forn(i, SIZE(adjList)){
-            if (visited[i] && levelOfNode[i] > currentLevel){
-                currentLevel = levelOfNode[i];
-                res = i;
-            }
-        }
-
-        return res;
-    }
-
-    int treeDiameter(){
-        int vertexOfDiameter = 
-    }
-
-}
-
-
-
-
-
-vector<int> getOneVertexOfDiameter(vector<int> &adjList){
-    int maxLevel = levelOfNode[0];
     int res = 0;
 
-    forn(i, SIZE(levelOfNode)){
-        if (levelOfNode[i] > levelOfNode[res]) res = i;
+    while (!q.empty()){
+        int u = q.front(); q.pop();
+        diameterVertex = u;
+
+        for (int w : adjList[u]){
+            if (!visited[w]){
+                q.push(w);
+                distances[w] = distances[u] + 1;
+                visited[w] = true;
+                res = max(res, distances[w]);
+            }
+        }
     }
 
     return res;
 }
 
-int getDiameterOfVertex(vector<vector<int>> &adjList, vector<int> &levelOfNode){
-    int vertexOfDiameter = getOneVertexOfDiameter(levelOfNode);
-    calculateLevels()
-
+int diameterTree(){
+    maxDepth(0);
+    int diameterVertex1 = diameterVertex;
+    int diameterLength = maxDepth(diameterVertex1);
+    int diameterVertex2 = diameterVertex;
+    return diameterLength;
 }
 
+// ###################################################################################################
+// Versión con DP
+void dfs(int v, int currentParentOfV){
+    vector<int> branchsLength;
+    
+    for (int u : adjList[v]){
+        if (u == currentParentOfV) continue;
+        dfs(u, v);
+        branchsLength.pb(longestBranchFrom[u]);
+    }
 
+    longestBranchFrom[v] = 0;
+    sort(all(branchsLength)); // Esto se puede hacer en O(n) buscando los dos máximos 
+    if (SIZE(branchsLength) > 0) longestBranchFrom[v] = 1 + branchsLength[SIZE(branchsLength)-1];
+    if (SIZE(branchsLength) > 1) longestPathThrough[v] = branchsLength[SIZE(branchsLength)-1] + 2 + branchsLength[SIZE(branchsLength)-2];
+    lengthDiameter = max(lengthDiameter, max(longestBranchFrom[v], longestPathThrough[v]));
+}
