291ce596d4ed3d09ecda52995a6eddd3c439b891
Romelai28 <Romelai28@gmail.com>
2025-07-29 06:02:29 -0300

    rolling hash

---
+++ notebook/strings/polynomial_rolling_hash.cpp
@@ -0,0 +1,20 @@
+// Precomputo: O(|s|)
+// Query: O(1)
+
+struct HashedString {	
+	HashedString(const string &s) : prefix_hash(SIZE(s)+1), potencia(SIZE(s)+1) {
+		potencia[0] = 1;
+		forsn(i, 1, SIZE(s)+1) { potencia[i] = mulMod(potencia[i-1], B, M); }
+		
+		prefix_hash[0] = 0;
+		forsn(i, 1, SIZE(s)+1) { prefix_hash[i] = addMod(mulMod(prefix_hash[i-1], B, M), (ll)s[i-1], M); }
+	}
+	.
+	ll get_hash(int start, int end){ // [start, end]
+		return mod(prefix_hash[end + 1] - mulMod(prefix_hash[start], potencia[end - start + 1], M), M);
+	}
+	
+private:
+	const ll M = ..., B = ...;  // Cambiar M y B.
+	vector<ll> prefix_hash, potencia;
+};
