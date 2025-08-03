e101944c8311da4fba8f4305c23d071aae839b2e
FacundoCG <coronelfacundo30@gmail.com>
2025-08-03 20:34:55 -0300

    Merge sort tree

---
+++ notebook/data_structures/merge_sort_tree.cpp
@@ -0,0 +1,59 @@
+template <typename T>
+using ordered_multiset = tree<
+    pair<T, int>,
+    null_type,
+    less<pair<T, int>>, // NOT less_equal
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+// Segment tree donde guardo los elementos de cada rango en los vértice de forma ordenada
+// Este segment tree te permite responder en un rango cuántos elementos k cumplen tq: x <= k <= y
+
+struct SegmentTree{
+    int n;
+    vl A;
+    ll elemNeutro;
+
+    vector<ordered_multiset<ll>> B;
+
+    SegmentTree(int N, vl &a, ll neutro) : n(N), A(a), elemNeutro(neutro){
+        B.resize(4*n);
+        build(1, 0, n-1);
+    }
+
+    ll f(int v, ll x, ll y){
+        // # elementos < y+1 y # elementos < x
+        ll res = B[v].order_of_key({y+1, -1}) - B[v].order_of_key({x, -1});
+        return res;
+    }
+
+    void build(int v, int tl, int tr){ // Vértice actual y rango [tl, tr] que indica este vértice
+        forsn(i, tl, tr+1) B[v].insert({A[i], i});
+        if (tl < tr) {
+            int tm = (tl + tr)/2;
+            build(2*v, tl, tm);
+            build(2*v+1, tm+1, tr); 
+        }
+    }
+
+    // O(log(n)^2)
+    // query(1, 0, n-1, l, r)
+    ll query(int v, int tl, int tr, int l, int r, ll x, ll y){
+        if (l > r) return elemNeutro; 
+        if (l == tl && r == tr) return f(v, x, y); // Respondo la query en este rango
+        int tm = (tl+tr)/2;
+        return query(2*v, tl, tm, l, min(r, tm), x, y) + query(2*v+1, tm+1, tr, max(l, tm+1), r, x, y);
+    }
+
+    void update(int v, int tl, int tr, int pos, ll new_val){
+        B[v].erase({A[pos], pos});
+        B[v].insert({new_val, pos});
+        if (tl < tr){
+            int tm = (tl + tr)/2;
+            if (pos <= tm) update(2*v, tl, tm, pos, new_val);
+            else update(2*v+1, tm+1, tr, pos, new_val);
+        }
+        A[pos] = new_val;
+    }
+};
--- notebook/data_structures/segment_tree.cpp
+++ notebook/data_structures/segment_tree.cpp
@@ -1,41 +1,47 @@
+// Segment tree básico para point update y range query de suma
+
 struct SegmentTree{
     int n;
     vl A, B;
+    ll elemNeutro;
 
-    SegmentTree(int N, vl &a) : n(N), A(a){
-        B.resize(4*n);
+    SegmentTree(int N, vl &a, ll neutro) : n(N), A(a), elemNeutro(neutro){
+        B.resize(4*n, elemNeutro);
         build(1, 0, n-1);
     }
 
+    ll combine(ll x, ll y){ return x + y;}
+    ll make_data(ll x){ return x;}
+
     void build(int v, int tl, int tr){ // Vértice actual y rango [tl, tr] que indica este vértice
-        if (tl == tr) B[v] = A[tl]; // Cuando llego a una hoja, el valor es el mismo elemento
+        if (tl == tr) B[v] = make_data(A[tl]); // Cuando llego a una hoja, el valor es el mismo elemento
         else {
             int tm = (tl + tr)/2;
-            // Construyo el hijo izquierdo y el derecho
             build(2*v, tl, tm);
-            build(2*v+1, tm+1, tr);
-            // B[v] = f(B[2*v], B[2*v+1]);
-            B[v] = B[2*v] + B[2*v+1];
+            build(2*v+1, tm+1, tr); 
+            B[v] = combine(B[2*v], B[2*v+1]);
         }
     }
 
     // query(1, 0, n-1, l, r)
     ll query(int v, int tl, int tr, int l, int r){
-        if (l > r) return 0; // Elemento neutro de las queries
+        if (l > r) return elemNeutro; 
         if (l == tl && r == tr) return B[v];
         int tm = (tl+tr)/2;
-        // res = f(query(2*v, tl, tm, l, min(r, tm)), query(2*v+1, tm+1, tr, max(l, tm+1), r))
-        return query(2*v, tl, tm, l, min(r, tm)) + query(2*v+1, tm+1, tr, max(l, tm+1), r);
+        return combine(query(2*v, tl, tm, l, min(r, tm)), query(2*v+1, tm+1, tr, max(l, tm+1), r));
     }
 
-    void update(int v, int tl, int tr, int pos, int new_val){
-        if (tl == tr) B[v] = new_val;
+    void update(int v, int tl, int tr, int pos, ll new_val){
+        if (tl == tr) B[v] = make_data(new_val);
         else {
             int tm = (tl + tr)/2;
             if (pos <= tm) update(2*v, tl, tm, pos, new_val);
             else update(2*v+1, tm+1, tr, pos, new_val);
-            // B[v] = f(B[2*v], B[2*v+1]);
-            B[v] = B[2*v] + B[2*v+1];
+            B[v] = combine(B[2*v], B[2*v+1]);
         }
     }
 };
+
+// #######################################################################################
+// Segment tree para range update y point query
+
