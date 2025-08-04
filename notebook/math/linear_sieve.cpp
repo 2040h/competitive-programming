ea95e457da701a1b2528c3574e53193d6ff7f3a0
FacundoCG <coronelfacundo30@gmail.com>
2025-08-04 19:57:49 -0300

    Fix de la linear sieve

--- notebook/math/linear_sieve.cpp
+++ notebook/math/linear_sieve.cpp
@@ -1,15 +1,21 @@
 const ll COTA_SUP_N = 110000;
-vl lp(COTA_SUP_N+1);
-vl primes;
+int lp[COTA_SUP_N+1];
+vi primes;
 
-void factorizar(){
+void linear_sieve(){
+	forsn(i, 1, COTA_SUP_N+1) lp[i] = 0;
+	
 	forsn(i, 2, COTA_SUP_N+1){
         if (lp[i] == 0){
-            lp[i] = i;
+			lp[i] = i;
             primes.pb(i);
         }
 
-        for(int j = 0; i*primes[j] <= N && primes[j] != lp[i]; j++) lp[i*primes[j]] = primes[j];
+        for(int j = 0; i*primes[j] <= COTA_SUP_N; j++) {
+			lp[i*primes[j]] = primes[j];
+			if (primes[j] == lp[i]) break;
+		}
     }
 }
+
 
