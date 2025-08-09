71932fb911c6373d943f838f92da87a58591b9e5
Romelai28 <Romelai28@gmail.com>
2025-08-08 22:10:36 -0300

    Actualizacion rolling hash. Version más robusta: Lazy + Multiples hashes + Randomización de bases.

--- notebook/strings/polynomial_rolling_hash.cpp
+++ notebook/strings/polynomial_rolling_hash.cpp
@@ -1,20 +1,40 @@
-// Precomputo: O(|s|)
-// Query: O(1)
+// Precomputo: O(ht*|s|)
+// Query: O(ht)
 
 struct HashedString {	
-	HashedString(const string &s) : prefix_hash(SIZE(s)+1), potencia(SIZE(s)+1) {
-		potencia[0] = 1;
-		forsn(i, 1, SIZE(s)+1) { potencia[i] = mulMod(potencia[i-1], B, M); }
-		
-		prefix_hash[0] = 0;
-		forsn(i, 1, SIZE(s)+1) { prefix_hash[i] = addMod(mulMod(prefix_hash[i-1], B, M), (ll)s[i-1], M); }
+	HashedString(const string &s) : prefix_hash(ht_size, vector<ll>(SIZE(s)+1)) {
+		forn(ht, ht_size){
+			const ll M = M_vec[ht], B = B_vec[ht];
+			
+			if(potencia.empty()) { potencia.assign(ht_size, vector<ll>(1, 1)); }
+			while(SIZE(potencia[ht]) <= SIZE(s)) { potencia[ht].pb(mulMod(potencia[ht].back(), B, M)); }
+			
+			prefix_hash[ht][0] = 0;
+			forsn(i, 1, SIZE(s)+1) { prefix_hash[ht][i] = addMod(mulMod(prefix_hash[ht][i-1], B, M), (ll)s[i-1], M); }
+		}
 	}
-	.
-	ll get_hash(int start, int end){ // [start, end]
-		return mod(prefix_hash[end + 1] - mulMod(prefix_hash[start], potencia[end - start + 1], M), M);
+	
+	vector<ll> get_hash(int start, int end){ // [start, end]
+		vector<ll> hash_res(ht_size);
+		forn(ht, ht_size){
+			const ll M = M_vec[ht];
+			hash_res[ht] = mod(prefix_hash[ht][end + 1] - mulMod(prefix_hash[ht][start], potencia[ht][end - start + 1], M), M);
+		}
+		return hash_res;
 	}
 	
 private:
-	const ll M = ..., B = ...;  // Cambiar M y B.
-	vector<ll> prefix_hash, potencia;
+	inline static const vector<ll> M_vec = {MOD, 991831889};
+	inline static vector<ll> B_vec = [](){
+		mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+		vector<ll> bases; bases.reserve(SIZE(M_vec));
+		for(ll M : M_vec){
+			const ll B = uniform_int_distribution<ll>(0.1 * M, 0.9 * M)(rng);
+			bases.pb(B);			
+		}
+		return bases;
+	}();
+	const int ht_size = SIZE(M_vec);
+	vv<ll> prefix_hash;
+	inline static vv<ll> potencia;
 };
---
+++ notebook/strings/polynomial_rolling_hash_simple.cpp
@@ -0,0 +1,23 @@
+// M = 1e9+7, 991831889, M=1e9+9 (Primo)
+// B=31, B=53, B=9973 (No necesariamente primo)
+
+// Precomputo: O(|s|)
+// Query: O(1)
+
+struct HashedString {	
+	HashedString(const string &s) : prefix_hash(SIZE(s)+1), potencia(SIZE(s)+1) {
+		potencia[0] = 1;
+		forsn(i, 1, SIZE(s)+1) { potencia[i] = mulMod(potencia[i-1], B, M); }
+		
+		prefix_hash[0] = 0;
+		forsn(i, 1, SIZE(s)+1) { prefix_hash[i] = addMod(mulMod(prefix_hash[i-1], B, M), (ll)s[i-1], M); }
+	}
+	
+	ll get_hash(int start, int end){ // [start, end]
+		return mod(prefix_hash[end + 1] - mulMod(prefix_hash[start], potencia[end - start + 1], M), M);
+	}
+	
+private:
+	const ll M = MOD, B = 31  // Cambiar M y B.  // M=991831889, M=1e9+9, B=53, B=9973.
+	vector<ll> prefix_hash, potencia;
+};
