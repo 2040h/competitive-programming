1360ade67c384b47d23e345afda1ee90efb88e17
FacundoCG <coronelfacundo30@gmail.com>
2025-08-15 10:20:19 -0300

    Cambio de base

---
+++ notebook/math/cambio_base.cpp
@@ -0,0 +1,11 @@
+vi convertFromDecimalToB(ll x, int b){ // Convierto de base 10 a base b
+	vi res; // Si está vacío, es 0
+	while (x > 0){ res.pb(x%b); x /= b;}
+    return res; // res[0] es el digito menos significativo
+}
+ 
+ll convertFromBToDecimal(vi &digits, int b){
+	ll res = 0;
+	forn(i, SIZE(digits)) res += digits[i]*((ll) pow(b, i));
+	return res;
+}
